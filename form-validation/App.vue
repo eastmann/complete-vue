b4906f9d15f793f2e5c4decf0fdc95ed83bde730
Maxim Klochkov <maxim.klochkov@gmail.com>
2022-11-11 23:43:10 +0300

    Form validation completed

--- form-validation/App.vue
+++ form-validation/App.vue
@@ -1,9 +1,68 @@
 <template>
-    App
+    <form @submit.prevent="submit">
+        <MyInput
+            name="Username"
+            type="text"
+            :rules="{ required: true, min: 5 }"
+            :value="username.value"
+            :error="username.error"
+            @update="update"
+        />
+        <MyInput
+            name="Password"
+            type="password"
+            :rules="{ required: true, min: 10 }"
+            :value="password.value"
+            :error="password.error"
+            @update="update"
+        />
+        <MyButton
+            background="darkslateblue"
+            color="white"
+            :disabled="!valid"
+        />
+    </form>
 </template>
 
 <script>
+import MyButton from './MyButton.vue';
+import MyInput from './MyInput.vue';
+
 export default {
+    components: {
+        MyButton,
+        MyInput
+    },
+
+    data() {
+        return {
+            username: {
+                value: 'user',
+                error: ''
+            },
+            password: {
+                value: 'pass',
+                error: ''
+            },
+        }
+    },
+
+    computed: {
+        valid() {
+            return !this.username.error && !this.password.error
+        }
+    },
+
+    methods: {
+        update({ name, value, error }) {
+            this[name].value = value
+            this[name].error = error
+        },
+
+        submit($event) {
+            console.log('Form submitted')
+        }
+    }
 }
 </script>
 
---
+++ form-validation/MyButton.vue
@@ -0,0 +1,45 @@
+<template>
+    <button
+        :style="{ background, color }"
+        :disabled="disabled"
+    >
+        Button
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        background: {
+            type: String
+        },
+        color: {
+            type: String
+        },
+        disabled: {
+            type: Boolean
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button {
+        margin-top: 10px;
+        padding: 10px 40px;
+
+        font-size: 16px;
+
+        border: none;
+        border-radius: 8px;
+        cursor: pointer;
+    }
+
+    button:disabled {
+        opacity: 0.5;
+    }
+
+    button:hover {
+        filter: brightness(125%);
+    }
+</style>
