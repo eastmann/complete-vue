f02afbf79a2ecbaed38fb1c66db2bf1fe15814e8
Maxim Klochkov <maxim.klochkov@gmail.com>
2022-11-12 23:03:25 +0300

    Create re-usable Card component; Implement Slots; Fetch Pokemon evolutions

--- pokemon/App.vue
+++ pokemon/App.vue
@@ -1,43 +1,76 @@
 <template>
     <div class="cards">
-        <div class="card"
-             v-for="pokemon in pokemons" :key="pokemon.id">
+        <Card
+            v-for="pokemon in pokemons"
+            :key="pokemon.id"
+            @click="fetchEvolutions(pokemon)"
+            :class="{ opace: pokemon.id !== selectedId }"
+            class="card"
+        >
+            <template v-slot:title>
+                {{ pokemon.name }} #{{ pokemon.id }}
+            </template>
 
-             <div class="title">
-                 {{ pokemon.name }}
-             </div>
+            <template v-slot:content>
+                <img :src="pokemon.sprite" :alt="pokemon.name" />
+            </template>
 
-             <div class="content">
-                 <img :src="pokemon.sprite" :alt="pokemon.name">
-             </div>
+            <template v-slot:description>
+                <div v-for="pokemon_type in pokemon.types" :key="pokemon_type">
+                    {{ pokemon_type }}
+                </div>
+            </template>
+        </Card>
+    </div>
 
-             <div class="description">
-                 <div v-for="pokemon_type in pokemon.types" :key="pokemon_type">
-                     {{ pokemon_type }}
-                 </div>
-             </div>
-        </div>
+    <div class="cards">
+        <Card
+            v-for="pokemon in evolutions"
+            :key="pokemon.id"
+        >
+            <template v-slot:title>
+                {{ pokemon.name }}
+            </template>
+
+            <template v-slot:content>
+                <img :src="pokemon.sprite" :alt="pokemon.name" />
+            </template>
+
+            <template v-slot:description>
+                <div v-for="pokemon_type in pokemon.types" :key="pokemon_type">
+                    {{ pokemon_type }}
+                </div>
+            </template>
+        </Card>
     </div>
 </template>
 
 <script>
 
+import Card from './Card.vue'
+
 const api = 'https://pokeapi.co/api/v2/pokemon'
-const ids = [1, 4, 7]
+const IDS = [1, 4, 7]
 
 export default {
+    components: {
+        Card
+    },
+
     data() {
         return {
-            pokemons: []
+            pokemons: [],
+            evolutions: [],
+            selectedId: null
         }
     },
 
-    created() {
-        this.fecthData()
+    async created() {
+        this.pokemons = await this.fecthData(IDS)
     },
 
     methods: {
-        async fecthData() {
+        async fecthData(ids) {
             const responses = await Promise.all(
                 ids.map(id => fetch(`${api}/${id}`))
             )
@@ -46,48 +79,46 @@ export default {
                 responses.map(response => response.json())
             )
 
-            this.pokemons = json.map(pokemon => ({
+            return json.map(pokemon => ({
                 id: pokemon.id,
                 name: pokemon.name,
                 sprite: pokemon.sprites.other['official-artwork'].front_default,
                 types: pokemon.types.map(type => type.type.name)
             }))
+        },
+
+        async fetchEvolutions(pokemon) {
+            this.evolutions = await this.fecthData(
+                [
+                    Number(`${pokemon.id}`) + 1,
+                    Number(`${pokemon.id}`) + 2
+                ]
+            )
+
+            this.selectedId = pokemon.id
+
+            // console.log(this.evolutions)
         }
     }
 }
 </script>
 
 <style scoped>
-
 img {
     max-width: 100%;
 }
-
 .cards {
     display: flex;
+    justify-content: center;
+
+    margin-top: 10px;
 }
-.card {
-    border: 1px solid silver;
-    border-radius: 8px;
-    max-width: 200px;
-    margin: 0 5px;
-    cursor: pointer;
-    box-shadow: 0px 1px 3px darkgrey;
-    transition: 0.2s;
+
+.opace {
+    opacity: 0.5;
 }
-.title, .content, .description {
-    padding: 16px;
-    text-transform: capitalize;
-    text-align: center;
-}
-.title, .content {
-    border-bottom: 1px solid silver;
-}
-.title {
-    font-size: 1.25em;
-}
+
 .card:hover {
-    transition: 0.2s;
-    box-shadow: 0px 1px 9px darkgrey;
+    opacity: 1;
 }
 </style>
