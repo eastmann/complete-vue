8b3c47469aff77e37637f24b896f8c6a1d20c807
Maxim Klochkov <maxim.klochkov@gmail.com>
2022-11-13 17:00:53 +0300

    Convert into Composable

--- composition/App.vue
+++ composition/App.vue
@@ -1,5 +1,4 @@
 <template>
-    <button @click="increment">{{ count }}</button>
     <button @click="a++">{{ a }}</button>
     <button @click="b++">{{ b }}</button>
     <p>{{ total }}</p>
@@ -14,54 +13,11 @@
 
 <script>
 import { ref, reactive, computed, watch, watchEffect } from 'vue'
+import { useNumbers } from './useNumbers.js'
 
 export default {
     setup() {
-        // ref -> numbers, string (single value)
-        const count = ref(0)
-        const increment = () => {
-            count.value++
-        }
-
-        const a = ref(1)
-        const b = ref(2)
-        const history = ref([])
-
-        // reactive -> {} or group values
-        // const numbers = reactive({
-        //     a: 1,
-        //     b: 2
-        // })
-
-        const total = computed(() => count.value + a.value + b.value)
-
-        // const increase = (value) => {
-        //     numbers[value]++
-        // }
-
-        // watch -> specify what you are going to watch -> more specific
-        watch([a, b], ([newA, newB], [oldA, oldB]) => {
-            if (newA !== oldA) {
-                history.value.push(`A: ${oldA} -> ${newA}`)
-            }
-            if (newB !== oldB) {
-                history.value.push(`B: ${oldB} -> ${newB}`)
-            }
-        })
-
-        // watchEffect -> pass whatever you like -> more general
-        // watchEffect(() => {
-        //     console.log(numbers.a)
-        // })
-
-        return {
-            a,
-            b,
-            count,
-            total,
-            increment,
-            history
-        }
+        return useNumbers()
     }
 }
 </script>
---
+++ composition/useNumbers.js
@@ -0,0 +1,25 @@
+import { ref, computed, watch } from 'vue'
+
+export function useNumbers() {
+    const a = ref(0)
+    const b = ref(0)
+    const history = ref([])
+
+    const total = computed(() => a.value + b.value)
+
+    watch([a, b], ([newA, newB], [oldA, oldB]) => {
+        if (newA !== oldA) {
+            history.value.push(`A: ${oldA} -> ${newA}`)
+        }
+        if (newB !== oldB) {
+            history.value.push(`B: ${oldB} -> ${newB}`)
+        }
+    })
+
+    return {
+        a,
+        b,
+        total,
+        history
+    }
+}
